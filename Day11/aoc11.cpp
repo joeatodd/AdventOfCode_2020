5bb5571154c45d53c0b0ba8aab7cfe5ed0fd31d1
J Todd <jat39@st-andrews.ac.uk>
2020-12-12 18:13:46 +0000

    Add OpenMP pragmas to Day 11

--- Day11/aoc11.cpp
+++ Day11/aoc11.cpp
@@ -8,6 +8,10 @@ using std::vector;
 
 typedef vector<vector<char>> seats_t;
 
+/* Rules for Pt 1:
+   - Unoccupied seats with no occupied neighbours become occupied
+   - Occupied seats with 4 or more occupied neighbours become empty
+*/
 char get_seat_next(seats_t &seats, int x, int y){
 
 	int nx = seats.at(0).size();
@@ -23,6 +27,7 @@ char get_seat_next(seats_t &seats, int x, int y){
 	// Check if empty or occupied
 	bool is_empty = seats[y][x] == 'L';
 
+	// Inspect all neighbours
 	for(int dy : {-1, 0, 1}){
 		for(int dx : {-1, 0, 1}){
 			if(dx == 0 && dy == 0) continue; // My seat
@@ -36,7 +41,7 @@ char get_seat_next(seats_t &seats, int x, int y){
 		}
 	}
 
-	// cout << "X " << x << "Y " << y << "empty: " << is_empty << " occ: " << neigh_occ << endl;
+	// Return new seat state based on rules
 	if(is_empty){
 		if(neigh_occ == 0){
 			return '#';
@@ -50,10 +55,15 @@ char get_seat_next(seats_t &seats, int x, int y){
 			return '#';
 		}
 	}
-
-
 }
 
+/* Rules for Pt 2:
+   - Unoccupied seats with no occupied neighbours become occupied
+   - Occupied seats with 5 or more occupied neighbours become empty
+   BUT!
+   - We consider the nearest visible seat in each direction (i.e.
+     look across empty floorspace.
+*/
 char get_seat_next_pt2(seats_t &seats, int x, int y){
 
 	int nx = seats.at(0).size();
@@ -75,6 +85,8 @@ char get_seat_next_pt2(seats_t &seats, int x, int y){
 
 			ty = y;
 			tx = x;
+
+			// Keep looking outward until we reach a seat
 			while(true){
 				ty += dy;
 				tx += dx;
@@ -90,7 +102,7 @@ char get_seat_next_pt2(seats_t &seats, int x, int y){
 		}
 	}
 
-	// cout << "X " << x << "Y " << y << "empty: " << is_empty << " occ: " << neigh_occ << endl;
+	// Return new seat state
 	if(is_empty){
 		if(neigh_occ == 0){
 			return '#';
@@ -108,6 +120,8 @@ char get_seat_next_pt2(seats_t &seats, int x, int y){
 
 }
 
+
+// Debugging output - show the current seating plan
 void print_seats(seats_t seat_arr){
 	for( auto row : seat_arr ){
 		for( auto s : row ){
@@ -123,6 +137,7 @@ bool unchanged(seats_t seats, seats_t seats_n){
 	return seats == seats_n;
 }
 
+// Return numbers of occupied seats
 int count_occ(seats_t seats){
 	int count = 0;
 	for (auto row : seats){
@@ -133,20 +148,22 @@ int count_occ(seats_t seats){
 
 	return count;
 }
+
 int main()
 {
 	std::ifstream infile("input.txt");
 	std::string line;
 	seats_t seat_arr, seat_arr_n, seat_arr_init;
 	vector<char> seat_row;
+	bool verbose = false;
 
+	// Read input
 	while(getline(infile, line)){
 		seat_row.clear();
 		for(auto l : line){
 			seat_row.push_back(l);
 		}
 		seat_arr_init.push_back(seat_row);
-		// cout << line << endl;
 	}
 
 	int nx = seat_arr_init.at(0).size();
@@ -155,19 +172,18 @@ int main()
 	seat_arr = seat_arr_init;
 	seat_arr_n = seat_arr;
 
+	// Iterate seat evolution until equilibrium
 	while(true){
+		#pragma omp parallel for collapse(2)
 		for(int i = 0; i < ny; i++){
 			for(int j = 0; j < nx; j++){
 				seat_arr_n.at(i).at(j) = get_seat_next(seat_arr, j, i);
-				// cout << "Change: " << j << " " << i << " " << seat_arr.at(i).at(j) << seat_arr_n.at(i).at(j) << endl;
 			}
 		}
 
-		// print_seats(seat_arr_n);
+		if(verbose) print_seats(seat_arr_n);
 		if(unchanged(seat_arr, seat_arr_n)) break;
 		seat_arr = seat_arr_n;
-
-		// break;
 	}
 
 	cout << "Part 1 answer: " << count_occ(seat_arr) << endl;
@@ -175,19 +191,18 @@ int main()
 	seat_arr = seat_arr_init;
 	seat_arr_n = seat_arr_init;
 
+	// Iterate to equilibrium
 	while(true){
+		#pragma omp parallel for collapse(2)
 		for(int i = 0; i < ny; i++){
 			for(int j = 0; j < nx; j++){
 				seat_arr_n.at(i).at(j) = get_seat_next_pt2(seat_arr, j, i);
-				// cout << "Change: " << j << " " << i << " " << seat_arr.at(i).at(j) << seat_arr_n.at(i).at(j) << endl;
 			}
 		}
 
-		// print_seats(seat_arr_n);
+		if(verbose) print_seats(seat_arr_n);
 		if(unchanged(seat_arr, seat_arr_n)) break;
 		seat_arr = seat_arr_n;
-
-		// break;
 	}
 
 	cout << "Part 2 answer: " << count_occ(seat_arr) << endl;
